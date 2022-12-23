6f91f20dad85bfd0847f792f987fb0fedd4f116c
Alexander Varwijk <alexander@getopensocial.com>
2022-12-23 18:59:51 +0100

    Test analysis of nodes using Behat
    This creates some tools for Behat so that we can create a node type in
    the `testing` Drupal profile and then test that the widget behaves as
    expected for a simple node.
    
    This test can be extended in the future to cover various reported bugs
    and integrations.

--- tests/features/behat.feature
+++
@@ -1,9 +0,0 @@
-@api @javascript
-Feature: Behat is working
-
-  Background:
-    Given module node is enabled
-
-  Scenario: We visit a page
-    Given I am logged in as a user with the "Administer content" permission
-    And I am on "/"
--- tests/src/Context/Setup.php
+++ tests/src/Context/Setup.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Drupal\Tests\yoast_seo\Context;
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Drupal\DrupalExtension\Context\DrushContext;
 
@@ -15,8 +16,20 @@ class Setup implements Context {
    * @BeforeScenario
    */
   public function installDrupal(BeforeScenarioScope $scope) : void {
-    $this->drushContext = $scope->getEnvironment()->getContext(DrushContext::class);
+    $environment = $scope->getEnvironment();
+    assert($environment instanceof InitializedContextEnvironment);
+    $drushContext = $environment->getContext(DrushContext::class);
+    assert($drushContext instanceof DrushContext);
+    $this->drushContext = $drushContext;
+
+    // Reset the config cache in our test runner since it may contain config
+    // from a previous scenario.
+    // @phpstan-ignore-next-line
+    \Drupal::configFactory()->reset();
+
+    // Install a fresh site for testing.
     $this->drushContext->assertDrushCommandWithArgument("site-install", "-y testing --site-name='Automated Behat Tests for Real-Time SEO' --account-name=admin --account-pass=admin --account-mail=admin@example.com");
+
     // We must enable the Olivero theme with block module so that we have a
     // logout link which is what is needed for DrupalContext to know whether
     // login succeeded.
